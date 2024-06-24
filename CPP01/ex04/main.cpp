06459d5146e53f56054a7f805e01774d87f5d09b
nevaspid <romain.brendle.guido@gmail.com>
2024-06-24 22:23:26 +0200

    ex04 done

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gloms <rbrendle@student.42mulhouse.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 15:46:14 by gloms             #+#    #+#             */
-/*   Updated: 2024/06/21 16:56:05 by gloms            ###   ########.fr       */
+/*   Updated: 2024/06/21 19:14:55 by gloms            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,75 +15,58 @@
 #include <cstdio>
 #include <string>
 
-bool copyFile(const std::string	&sourcePath, const std::string	&destPath) // je passe en const car je ne modifierai jms les valeurs
+bool	replaceInFile(const std::string sourceFile, const std::string renamedFile,
+					const std::string toFind, const std::string toReplace)
 {
-    std::ifstream source(sourcePath.c_str(), std::ios::binary);
-    std::ofstream dest(destPath.c_str(), std::ios::binary);
+	std::ifstream file(sourceFile.c_str());
+	std::ofstream outFile(renamedFile.c_str());
 
-	if (!source.is_open() || !dest.is_open())
+	if (!file.is_open())
 	{
-        std::cerr << "Error opening files" << std::endl;
+		std::cerr << "Error while opening file" << std::endl;
+		return false;
+	}
+	if (!outFile.is_open())
+	{
+        std::cerr << "Error while creating file" << std::endl;
         return false;
     }
-    dest << source.rdbuf();
-    source.close();
-    dest.close();
-
-    return true;
-}
-
-void replaceInFile(const std::string &sourceFile, const std::string &from, const std::string &to)
-{
-    std::ifstream source(sourceFile.c_str());
-    std::ofstream temp((sourceFile + ".temp").c_str());
-    std::string line;
-
-    while (std::getline(source, line))
+	std::string line;
+	const std::string &refLine = line;
+	std::string newLine;
+	size_t		pos;
+	size_t		start = 0;
+	while (std::getline(file, line))
 	{
-        std::string newLine;
-        size_t startPos = 0;
-        size_t foundPos;
-        while ((foundPos = line.find(from, startPos)) != std::string::npos)
+		while ((pos = refLine.find(toFind, start)) != std::string::npos)
 		{
-            newLine += line.substr(startPos, foundPos - startPos) + to;
-            startPos = foundPos + from.length();
-        }
-        newLine += line.substr(startPos);
-        temp << newLine << std::endl;
-    }
-
-    source.close();
-    temp.close();
-
-    std::remove(sourceFile.c_str());
-    std::rename((sourceFile + ".temp").c_str(), sourceFile.c_str());
+		    newLine += refLine.substr(start, pos - start) + toReplace;
+		    start = pos + toFind.length();
+		}
+		newLine += refLine.substr(start); // Ajouter le reste de la ligne après la dernière occurrence
+		outFile << newLine << std::endl;
+		pos = 0;
+		start = 0;
+		newLine.erase();
+		line.erase();
+	}
+	return true;
 }
 
 int main(int ac, char **av)
 {
     if (ac != 4)
 	{
-        std::cerr << "Usage: " << av[0] << " <source_file> <to_find> <to_replace>" << std::endl;
+        std::cerr << "Usage: " << av[0] << " <source_file> <toFind> <toReplace>" << std::endl;
         return 1;
     }
 
-    std::string sourceFile = av[1];
-    std::string destFile = sourceFile + ".temp";
-    std::string renamedFile = sourceFile + ".replace";
+	const std::string sourceFile = av[1];
+	const std::string renamedFile = sourceFile + ".replace";
+	const std::string toFind = av[2];
+	const std::string toReplace = av[3];
 
-    if (copyFile(sourceFile, destFile))
-	{
-        std::cout << "File copied successfully." << std::endl;
-        if (std::rename(destFile.c_str(), renamedFile.c_str()) == 0)
-		{
-            std::cout << "File renamed successfully to " << renamedFile << std::endl;
-        }
-		else
-            std::cerr << "Error renaming file." << std::endl;
-    }
-	else
-        std::cout << "Failed to copy the file." << std::endl;
-	replaceInFile(renamedFile, av[2], av[3]);
-
-    return 0;
+	if (replaceInFile(sourceFile, renamedFile, toFind, toReplace))
+		return 0;
+	return 1;
 }
