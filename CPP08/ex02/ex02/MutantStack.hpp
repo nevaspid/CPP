8ac6d81d2ee5df1df2b4ce5a919355c3a5a094b1
gloms <109989668+nevaspid@users.noreply.github.com>
2025-01-06 12:16:29 +0100

    Add files via upload

---
+++ CPP08/ex02/ex02/MutantStack.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gloms <rbrendle@student.42mulhouse.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/23 21:29:07 by gloms             #+#    #+#             */
+/*   Updated: 2025/01/06 11:20:05 by gloms            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stack>
+#include <deque>
+#include <iostream>
+
+template <typename T>
+class MutantStack : public std::stack<T> {
+	public :
+
+	private :
+}
